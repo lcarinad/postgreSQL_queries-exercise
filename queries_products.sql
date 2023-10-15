0719246df5b778f068a5166e7ffc821764f4e253
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-10-15 14:07:17 -0400

    queries for products_db

---
+++ queries_products.sql
@@ -0,0 +1,30 @@
+-- Comments in SQL Start with dash-dash --
+--1️⃣Add a product to the table with the name of “chair”, price of 44.00, and can_be_returned of false.
+--INSERT INTO products (name, price, can_be_returned)                                                                          VALUES('chair', 44.00, false);--
+--2️⃣Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
+--INSERT INTO  products (name, price, can_be_returned) VALUES('stool', 25.99, true);--
+--3️⃣Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
+--INSERT INTO products (name, price, can_be_returned) VALUES('table', 124.00, false);
+--4️⃣Display all of the rows and columns in the table.
+-- SELECT * FROM products;
+--5️⃣Display all of the names of the products.
+--SELECT name FROM products;
+--6️⃣Display all of the names and prices of the products.
+--SELECT name, price FROM products;
+--7️⃣Add a new product - make up whatever you would like!
+--INSERT INTO products (name, price, can_be_returned) VALUES ('accent chair', 175.00, true);
+--8️⃣Display only the products that can_be_returned
+--SELECT * FROM products WHERE can_be_returned = true;
+--9️⃣Display only the products that have a price less than 44.00.
+--SELECT * FROM products WHERE price < 44;
+--🔟Display only the products that have a price in between 22.50 and 99.99.
+--SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+--1️⃣1️⃣There’s a sale going on: Everything is $20 off! Update the database accordingly.
+--UPDATE products
+--SET price = price - 20;
+--1️⃣2️⃣Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+--DELETE from products WHERE price < 25;
+--1️⃣3️⃣ And now the sale is over. For the remaining products, increase their price by $20.
+--UPDATE products SET price = price + 20;
+--1️⃣4️⃣There is a new company policy: everything is returnable. Update the database accordingly.
+--UPDATE products SET can_be_returned = true;
